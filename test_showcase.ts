735003035194a5c24fcb32a72f9526b4297af943
cloedesignmilan-2768 <cloe.design.milan@gmail.com>
2026-04-23 10:50:55 +0200

    Fix build: remove test_showcase.ts

--- test_showcase.ts
+++
@@ -1,9 +0,0 @@
-import { getShowcaseData } from './src/lib/getShowcaseData.ts';
-
-async function test() {
-  const data = await getShowcaseData();
-  const swimwear = data.find(d => d.id === 'swimwear-e-commerce-clean');
-  console.log(JSON.stringify(swimwear, null, 2));
-}
-
-test();
