49a5e2098263b7c31395cf7f8c6c8086f1a67169
cloedesignmilan-2768 <cloe.design.milan@gmail.com>
2026-04-24 10:59:15 +0200

    UI: Enable Apple Sign In button

--- src/app/auth/AuthButtons.tsx
+++ src/app/auth/AuthButtons.tsx
@@ -46,8 +46,6 @@ export default function AuthButtons() {
         Sign in with Google
       </button>
 
-      {/* Placeholder for Apple - Can be enabled later in Supabase Dashboard */}
-      {/* 
       <button 
         onClick={() => handleOAuthLogin('apple')}
         disabled={loading}
@@ -59,9 +57,11 @@ export default function AuthButtons() {
           boxShadow: '0 4px 6px rgba(0,0,0,0.2)'
         }}
       >
+        <svg viewBox="0 0 24 24" width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+          <path fill="#ffffff" d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.62-1.48 3.608-2.925 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.502 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.68.727-1.303 2.155-1.108 3.53 1.344.104 2.553-.506 3.395-1.518z"/>
+        </svg>
         Sign in with Apple
-      </button> 
-      */}
+      </button>
     </div>
   )
 }
