fee2b373988c58faff5869993a63d85a2c8bf276
cloedesignmilan-2768 <cloe.design.milan@gmail.com>
2026-04-29 10:05:15 +0200

    fix: await cookies in i18n server action

--- src/app/actions/i18n.ts
+++ src/app/actions/i18n.ts
@@ -4,5 +4,6 @@ import { cookies } from 'next/headers';
 import { Locale } from '@/lib/i18n/dictionaries';
 
 export async function setLocale(locale: Locale) {
-  cookies().set('NEXT_LOCALE', locale, { path: '/', maxAge: 60 * 60 * 24 * 365 });
+  const cookieStore = await cookies();
+  cookieStore.set('NEXT_LOCALE', locale, { path: '/', maxAge: 60 * 60 * 24 * 365 });
 }
